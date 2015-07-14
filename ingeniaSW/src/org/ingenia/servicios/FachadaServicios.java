749034e12fa89a4da9f982e5cec77a410500e75c
Sergio Bayona <sergioalejo@gmail.com>
2015-07-14 05:29:22 -0500

    Subiendo trabajo previo a ingeniaSW

--- ingeniaSW/src/org/ingenia/servicios/FachadaServicios.java
+++ ingeniaSW/src/org/ingenia/servicios/FachadaServicios.java
@@ -1,9 +1,12 @@
 package org.ingenia.servicios;
 
+import java.util.ArrayList;
+
 import javax.ejb.EJB;
 
 import org.ingenia.comunes.excepcion.AdaptadorException;
 import org.ingenia.comunes.vo.ActividadVO;
+import org.ingenia.comunes.vo.EstructuraVO;
 import org.ingenia.negocio.igestor.IGestorActividadesRemote;
 
 public class FachadaServicios {
@@ -25,4 +28,19 @@ public class FachadaServicios {
 		return actividadVO;
 	}
 
+	public ArrayList<EstructuraVO> ConsultaEstructurasPorActividad(String idActividad) {
+		ArrayList<EstructuraVO> estructuras = new ArrayList<EstructuraVO>();
+		ActividadVO actividadVO = new ActividadVO();
+		if (idActividad != null) {
+			actividadVO.setIdactividad(Integer.parseInt(idActividad));
+		}
+		
+		try {
+			actividadVO = gestorActividad.consultarActividad(actividadVO);
+		} catch (AdaptadorException e) {
+			e.printStackTrace();
+		}
+		return estructuras;
+	}  
+	
 }
--- ingeniaSW/src/org/ingenia/servicios/actividad/GestorServicioWS.java
+++ ingeniaSW/src/org/ingenia/servicios/actividad/GestorServicioWS.java
@@ -1,22 +1,25 @@
 package org.ingenia.servicios.actividad;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.ingenia.comunes.vo.ActividadVO;
+import org.ingenia.comunes.vo.EstructuraVO;
+import org.ingenia.servicios.FachadaServicios;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/ingenia")
+@RequestMapping("/service")
 public class GestorServicioWS {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(GestorServicioWS.class);
+	private static final Logger logger = LoggerFactory.getLogger(GestorServicioWS.class);
 
 	Map<Integer, ActividadVO> actividades = new HashMap<Integer, ActividadVO>();
 
@@ -27,13 +30,38 @@ public class GestorServicioWS {
 		actividad.setActivo(0);
 		actividad.setEnunciado("enunciado");
 		actividad.setLimite_movimientos(1);
-		actividad.setId_juego(0);
+		//actividad.setId_juego(0);
 
 		actividades.put(9999, actividad);
 
 		logger.debug("Se ejecuta OK el Dummy de servicio rest de actividades !!!");
 
 		return actividad;
+	}
+	
+	//@RequestMapping(value = "/{IdActividad}", method = RequestMethod.GET)
+	@RequestMapping(value = GIngeniaURI.GET_ACTIVIDAD, method = RequestMethod.GET)
+	public ActividadVO getActividad(@PathVariable String IdActividad) {
+		//consulta a BD
+		ActividadVO actividad = new ActividadVO(); 		
 		
+		FachadaServicios fachada = new FachadaServicios();
+		 
+		actividad = fachada.consultaActividadVO(IdActividad);
+			
+        return actividad;
+	}
+	
+	@RequestMapping(value = GIngeniaURI.GET_ACTIVIDAD, method = RequestMethod.GET)
+	public ArrayList<EstructuraVO> getEstructurasPorActividad(@PathVariable String IdActividad) {
+		
+		ArrayList<EstructuraVO> estructuras = new ArrayList<EstructuraVO>(); 		
+		
+		//consulta a BD
+		
+		FachadaServicios fachada = new FachadaServicios();
+		estructuras =  fachada.ConsultaEstructurasPorActividad(IdActividad);
+			
+        return estructuras;
 	}
 }
