c9960cb383c3d5386a66734cca6fab7916f58e98
sergiob2 <sergioalejo@gmail.com>
2015-07-21 01:46:04 +0800

    Se eliminan scripts innecesarios.

--- ingeniaWEB/WebContent/pluggins/CatCraft/js/init/loader.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	//creamos el canvas para el juego
-	var htmlCanvas = '<canvas id="canvas" width="800" height="500">Your browser does not support the HTML5 canvas tag.</canvas>';
-	$("#ContenidoActividad").html(htmlCanvas);
-     var controlador = new ControladorJuego();
-     controlador.CargaInicial();
-});
