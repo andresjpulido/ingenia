f9fa3014bc39defcd7bc9b1fa4a9e6c3a3f0ed38
manucdlm <ingenierocastroud@gmail.com>
2015-07-29 08:01:10 -0500

    se actualiza el orden de salida de los gatos despues de eliminarse, se agrega imagen de ninguna y se cambio valor del campo en el script Bas
    se actualiza el orden de salida de los gatos despues de eliminarse,se
    agrega imagen de ninguna y se cambio valor del campo en el script Base
    cargue

--- scripts/ingenia/basecargue.sql
+++ scripts/ingenia/basecargue.sql
@@ -1,7 +1,7 @@
 ﻿use ingenia;
 
 INSERT INTO `arma` (`idarma`,`nombre_arma`,`puntaje`) VALUES (1,'espada',10),(2,'hacha',10),(3,'zarpa',10),(4,'puño',10),(5,'patada',10),(6,'arco',10),(7,'bate',10),(8,'bomba',10),(9,'cañon',10),(10,'hado-ken',10),(11,'laser',10),(12,'martillo',10),(13,'mazo',10),(14,'perro',10),(15,'pistola',10),(16,'sierra',10);
-INSERT INTO `armadura` VALUES (1,'tela',10),(2,'cuero',10),(3,'malla',10),(4,'sin_armadura',10);
+INSERT INTO `armadura` VALUES (1,'tela',10),(2,'cuero',10),(3,'malla',10),(4,'ninguna',10);
 INSERT INTO `color` VALUES (1,'azul',10),(2,'verde',10),(3,'rojo',10),(4,'amarillo',10);
 INSERT INTO `estructura` VALUES (1,'if'),(2,'while'),(3,'for'),(4,'switch'),(5,'or'),(6,'and'),(7,'mayor que'),(8,'igual que '),(9,'menor que'),(10,'diferente que');
 INSERT INTO `juego` VALUES (1,'gato','catcraft',"");
